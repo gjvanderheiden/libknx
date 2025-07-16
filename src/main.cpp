3cca65577e6b35650502ea1fcb55ba21e23f5766
Gert-Jan van der Heiden <gjvdspam@gmail.com>
2025-07-16 12:24:40 +0200

    local bind ip address is a command argument now

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,13 @@ int main(int argc, char *argv[]) {
   for (auto router : discovery.result()) {
     std::cout << "Found router " << router.name << std::endl;
     std::cout << "with ip address " << router.ip << std::endl;
-
-    asio::io_context io_context;
-    connection::IpKnxConnection connection{io_context, router.ip, router.port, "192.168.10.114"};
-    asio::co_spawn(io_context, connection.start(), asio::detached);
-    io_context.run();
+    if (argc == 2) {
+      asio::io_context io_context;
+      connection::IpKnxConnection connection{io_context, router.ip, router.port,
+                                             argv[1]};
+      asio::co_spawn(io_context, connection.start(), asio::detached);
+      io_context.run();
+    }
   }
   return 0;
 }
